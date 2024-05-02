22b8616eed5a9d2a557d9b5600c532917039e5fb
msi <344876270@qq.com>
2024-05-02 20:31:23 +0800

    2024/5/2

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -6,18 +6,16 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-card>
+        <IonCardHeader>
+          11111111112222222222
+        </IonCardHeader>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
